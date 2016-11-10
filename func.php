184819f25cff9c1cf5fec18a90c6d8d6d75013dd
Fhazkard <owner@fhazkard.com>
2016-11-11 00:14:40 +0700

    update new code
    done simple webservice sistem download

--- func.php
+++ func.php
@@ -1,25 +1,41 @@
 <?php
-function proses_data($data){
-	require 'konek.php';
-	$list = array('0'=>array('NULL',NULL));
-	$selek_data = mysqli_query($koneksi,"SELECT * FROM MURID");
+include 'konek.php';
+$list = array();
+
+function selek_data($id){
+	$selek_data = mysqli_query($GLOBALS['koneksi'],"SELECT bimbel.nama AS bimbel, murid.nama AS murid, materi.sekolah_id, materi.kelas_id, materi.bimbel_id, 
+													materi.judul AS judul, materi.keterangan AS keterangan, materi.tgl_terbit AS tgl_terbit, materi.file_location AS link_download FROM MURID 
+													JOIN bimbel ON bimbel.bimbel_id = murid.bimbel_id
+													JOIN materi ON materi.bimbel_id = murid.bimbel_id AND materi.kelas_id = murid.kelas_id
+													WHERE murid.murid_id = '".$id."'");
 	while($row = mysqli_fetch_array($selek_data))
 	{
-		array_push($list,array($row['nama'],$row['tlp']));
+		$link = generate_link($row['link_download']);
+		array_push($GLOBALS['list'],array('Bimbel'=>$row['bimbel'],'Judul'=>$row['judul'],'Keterangan'=>$row['keterangan'],'Tanggal'=>$row['tgl_terbit'],'Link'=>$link));
 	}	
-	foreach($list as $item=>$value){
-		if($item == $data){
-			return $value;
-			break;
-		}
-	}
 }
 
-function show_data($status,$status_message,$data){
-	header("HTTP/1.1 $status $status_message");
+function send_data(){
+	$item = count($GLOBALS['list']);
+	if($item==0){
+		array_push($GLOBALS['list'],array('Bimbel'=>'-','Judul'=>'-','Keterangan'=>'-','Tanggal'=>'-','Link'=>'#'));
+		return $GLOBALS['list'];
+	}else{
+		return $GLOBALS['list'];
+	}	
+}
+
+function generate_link($link){
+	$link = substr($link,37);
+	$link = "../../koneksi/".$link;
+	return $link;
+}
+
+function show_data($status,$notice,$data){
+	header("HTTP/1.1 $status $notice");
 	$respone['Status'] = $status;
-	$respone['Nama'] = $status_message;
-	$respone['No Hp'] =  $data;
+	$respone['Notice'] = $notice;
+	$respone['Data'] = $data;
 	
 	$result = json_encode($respone);
 	echo $result;
--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 include 'func.php';
-if(!empty($_GET['id'])){
-	$id = $_GET['id'];
-	$value = proses_data($id);
-	if(empty($value)){
-		show_data(200,'Name Not Found',NULL);
-	}else{
-		show_data(200,$value[0],$value[1]);
-	}
+if(!empty($_REQUEST['id'])){
+	$id=$_REQUEST['id'];
+	selek_data($id);
+	$value = send_data();
+	if(!empty($value)){
+		show_data(200,'Data Found',$value);
+	}			
 }else{
-	show_data(400,'Service Not Found',NULL);
+	show_data(400,'Service Not Found','NULL');
 }
 
+/* for($i = 0; $i < count($value);$i++){
+	print_r($value[$i]['Keterangan']);
+	echo PHP_EOL;
+} */
+/* foreach($value as $item){
+		echo $item." ";
+		echo PHP_EOL;
+	} */
 ?>
